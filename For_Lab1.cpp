cbb31dddd713034994da82aa175df72a98e6cefb
Not-Saint-Paul <145997767+Not-Saint-Paul@users.noreply.github.com>
2023-10-05 18:33:57 +0700

    Update For_Lab1.cpp

--- For_Lab1.cpp
+++ For_Lab1.cpp
@@ -1,8 +1,8 @@
 /************************************
  * Автор: Станковский П.А.          *
- * Дата: 27.09.2023                 *
+ * Дата: 05.10.2023                 *
  * Название: Shitcode Lab1 v2       *
- * https://onlinegdb.com/0jgTzWWlg  *
+ * https://onlinegdb.com/KymjP_DOu  *
  ************************************/
 #include <iostream>
 #include <math.h>
@@ -10,12 +10,11 @@
 using namespace std;
 
 int main() {
-    double g = 9.81, Mass = 25.52, angle = 0.698132, k = 0.15, mass = 5.7;
-    double a1, a2, whatisthAt;
+    double g = 9.81, Mass = 25.52, angle = 0.698132, k = 0.15, mass = 5.7, a1, a2, whatisthAt;
     
-    whatisthAt = Mass + mass*(pow(sin(angle), 2) - k*(sin(2*angle)/2));
-    a1 = (g * Mass / whatisthAt) * (cos(angle)*(sin(angle) - k*cos(angle)));
-    a2 = (g * Mass / whatisthAt) * (sin(angle)*(cos(angle) + k*sin(angle)));
+    whatisthAt = Mass + mass * (pow(sin(angle), 2) - k * (sin(2*angle) / 2));
+    a1 = (g * Mass / whatisthAt) * (cos(angle) * (sin(angle) - k * cos(angle)));
+    a2 = (g * Mass / whatisthAt) * (sin(angle) * (cos(angle) + k * sin(angle)));
     
     cout.fixed;
     cout << "a1 = " << a1 << ";  a2 = " << a2 << endl;
